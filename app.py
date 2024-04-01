990cdbff666eb74fa9ecbd24a00f5bee4783d796
GabrielGarciaRodri <145493679+GabrielGarciaRodri@users.noreply.github.com>
2024-04-01 00:40:03 -0500

    feat: :sparkles: Funtions updated
    New routes added for all html files

--- app.py
+++ app.py
@@ -5,10 +5,38 @@ from flask import Flask, render_template
 app = Flask (__name__)
 
 
+#RUTA DE LAS IMAGENES
+
+
+
 #RUTAS
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/views/abrigos")
+def abrigos():
+    return render_template('/views/abrigos.html')
+
+@app.route("/views/camisetas")
+def camisetas():
+    return render_template("/views/camisetas.html")
+
+@app.route("/views/pantalones")
+def pantalones():
+    return render_template("/views/pantalones.html")
+
+@app.route("/views/carrito")
+def carrito():
+    return render_template("/views/carrito.html")
+
+@app.route("/views/carritoVacio")
+def carritoVacio():
+    return render_template("carritoVacio.html")
+
+@app.route("/views/carritoLleno")
+def carritoLleno():
+    return render_template("carritoLleno.html")
 
 @app.route("/contacto")
 def contacto():
